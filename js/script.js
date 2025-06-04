169d72a5db2aed55c8f34ba2392088c1bc597bcb
Abis Figueredo <afigueredo@ultracem.co>
2025-06-03 19:16:05 -0500

    Adición de ruta para funcionalidad en GitHub

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 document.querySelector(".button-start").addEventListener("click", function() {
     const mainNext = document.querySelector(".main");
-    const soundGo = new Audio (src="/sound/go-lets-go.mp3");
+    const soundGo = new Audio (src="/Saving_the_Princess/sound/go-lets-go.mp3");
     
     
     // Agregar clase para el efecto de desvanecimiento
@@ -12,6 +12,6 @@ document.querySelector(".button-start").addEventListener("click", function() {
     
     // Esperar el tiempo de la animación antes de redirigir
     setTimeout(() => {
-        window.location.href = "/game/game.html"; // Redirigir a la pagina del juego, sin cambiar de pestaña
+        window.location.href = "/Saving_the_Princess/game/game.html"; // Redirigir a la pagina del juego, sin cambiar de pestaña
     }, 1900); //Tiempo de retraso para cargar la pagina del juego
 });
